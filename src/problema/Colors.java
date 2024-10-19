850a2527e6913946139c8d462dd8b4b526cfcc72
Vitor Santos <the_vitor@hotmail.com>
2024-10-18 22:23:42 -0300

    versao final produtor-consumidor

---
+++ src/problema/Colors.java
@@ -0,0 +1,7 @@
+package problema;
+
+public class Colors {
+     public static final String RESET = "\033[0m";  // Text Reset
+     public static final String RED_BOLD = "\033[1;31m";    // RED
+     public static final String GREEN_BOLD = "\033[1;32m";  // GREEN
+}
---
+++ src/problema/ReadWriter.java
@@ -0,0 +1,22 @@
+package problema;
+
+import java.util.concurrent.Semaphore;
+
+public class ReadWriter {
+    static Semaphore bookShelf_acess = new Semaphore(1);    // controla o acesso à estante de livro
+    static String[] books = new String[10];
+
+    public static void main(String[] args) {
+        Reader reader1 = new Reader(bookShelf_acess, books);
+        Writer writer1 = new Writer(bookShelf_acess, books);
+        Reader reader2 = new Reader(bookShelf_acess, books);
+        Writer writer2 = new Writer(bookShelf_acess, books);
+
+        reader2.start();
+        reader1.start();
+        writer1.start();
+        writer2.start();
+        
+        
+    }
+}
